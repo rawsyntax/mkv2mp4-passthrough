4889772ac2193ce5258553ca9c34b583131255a1
Eric Himmelreich <eahimmel@gmail.com>
2010-11-20 11:55:18 -0500

    simple bash script handles basic case converting mkv to mp4

---
+++ convert.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+fname=$1
+
+if [ -f $1 ] ; then
+    filename=$(basename $fname)
+    extension=${filename##*.}
+    name=${filename%.*}
+    echo "converting ${name} to mp4"
+    `mkvextract tracks ${fname} 1:${name}.h264 2:${name}.ac3 > /dev/null 2>&1`
+    `a52dec ${name}.ac3 -o wavdolby > ${name}.wav`
+    `faac ${name}.wav > /dev/null 2>&1`
+    FPS="23.976"
+    `MP4Box -add ${name}.aac -add ${name}.h264 -fps $FPS ${name}.mp4 > /dev/null 2>&1`
+    `rm ${name}.aac ${name}.ac3 ${name}.h264 ${name}.wav`
+fi
