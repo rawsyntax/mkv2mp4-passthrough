d83b705d80062b8000adfa515863f32742f740c2
Eric Himmelreich <eahimmel@gmail.com>
2010-12-01 23:05:17 -0500

    fix audio issues (m4a instead of aac), itunes / quicktime is picky like that

--- convert.sh
+++ convert.sh
@@ -8,8 +8,8 @@ if [ -f $1 ] ; then
     echo "converting ${name} to mp4"
     `mkvextract tracks ${fname} 1:${name}.h264 2:${name}.ac3 > /dev/null 2>&1`
     `a52dec ${name}.ac3 -o wavdolby > ${name}.wav`
-    `faac ${name}.wav > /dev/null 2>&1`
+    `faac ${name}.wav -o ${name}.m4a` 
     FPS=`mediainfo --Inform=Video\;%FrameRate% "${fname}"`
-    `MP4Box -add ${name}.aac -add ${name}.h264 -fps $FPS ${name}.mp4 > /dev/null 2>&1`
-    `rm ${name}.aac ${name}.ac3 ${name}.h264 ${name}.wav`
+    `MP4Box -add ${name}.m4a -add ${name}.h264 -fps $FPS ${name}.mp4 > /dev/null 2>&1`
+    `rm ${name}.ac3 ${name}.m4a ${name}.h264 ${name}.wav`
 fi
