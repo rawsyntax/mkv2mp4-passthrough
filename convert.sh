036464eb2923bfbb23bfd3de1f0af2a37b65f220
Eric Himmelreich <eahimmel@gmail.com>
2010-12-10 16:52:25 -0500

    more cleanup

--- convert.sh
+++ convert.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
-fname=$1
 
 if [ -f $1 ] ; then
-    filename=$(basename $fname)
+    filename=$(basename $1)
     extension=${filename##*.}
     name=${filename%.*}
-    VIDEO=`mediainfo --Inform=Video\;%ID% "${fname}"`
-    AUDIO=`mediainfo --Inform=Audio\;%ID% "${fname}"`
-    `mkvextract tracks ${fname} ${VIDEO}:${name}.h264 ${AUDIO}:${name}.ac3`
+    video=`mediainfo --Inform=Video\;%ID% "${fname}"`
+    audio=`mediainfo --Inform=Audio\;%ID% "${fname}"`
+    fps=`mediainfo --Inform=Video\;%FrameRate% "${fname}"`
+    `mkvextract tracks ${fname} ${video}:${name}.h264 ${audio}:${name}.ac3`
     `a52dec ${name}.ac3 -o wavdolby > ${name}.wav`
     `faac ${name}.wav -o ${name}.m4a`
-    FPS=`mediainfo --Inform=Video\;%FrameRate% "${fname}"`
-    `MP4Box -add ${name}.m4a -add ${name}.h264 -fps $FPS ${name}.mp4`
+    `MP4Box -add ${name}.m4a -add ${name}.h264 -fps $fps ${name}.mp4`
     `rm ${name}.m4a ${name}.ac3 ${name}.h264 ${name}.wav ${name}.mkv`
 fi
