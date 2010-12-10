dfb4be98c4f86968b38a2091911844904da9ce19
Eric Himmelreich <eahimmel@gmail.com>
2010-12-09 22:19:22 -0500

    script detects audio/video track numbers

--- convert.sh
+++ convert.sh
@@ -6,10 +6,12 @@ if [ -f $1 ] ; then
     extension=${filename##*.}
     name=${filename%.*}
     echo "converting ${name} to mp4"
-    `mkvextract tracks ${fname} 1:${name}.h264 2:${name}.ac3 > /dev/null 2>&1`
+    VIDEO_TRACK=`mediainfo --Inform=Video\;%ID% "${fname}"`
+    AUDIO_TRACK=`mediainfo --Inform=Audio\;%ID% "${fname}"`
+    `mkvextract tracks ${fname} ${VIDEO_TRACK}:${name}.h264 ${AUDIO_TRACK}:${name}.ac3 > /dev/null 2>&1`
     `a52dec ${name}.ac3 -o wavdolby > ${name}.wav`
-    `faac ${name}.wav -o ${name}.m4a` 
+    `faac ${name}.wav -o ${name}.m4a`
     FPS=`mediainfo --Inform=Video\;%FrameRate% "${fname}"`
     `MP4Box -add ${name}.m4a -add ${name}.h264 -fps $FPS ${name}.mp4 > /dev/null 2>&1`
-    `rm ${name}.ac3 ${name}.m4a ${name}.h264 ${name}.wav`
+    `rm ${name}.m4a ${name}.ac3 ${name}.h264 ${name}.wav ${name}.mkv`
 fi
