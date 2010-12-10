61aef54d3da4bdd9f38676168a2463f5fed8ded5
Eric Himmelreich <eahimmel@gmail.com>
2010-12-10 15:17:59 -0500

    cleanup script

--- convert.sh
+++ convert.sh
@@ -5,13 +5,12 @@ if [ -f $1 ] ; then
     filename=$(basename $fname)
     extension=${filename##*.}
     name=${filename%.*}
-    echo "converting ${name} to mp4"
-    VIDEO_TRACK=`mediainfo --Inform=Video\;%ID% "${fname}"`
-    AUDIO_TRACK=`mediainfo --Inform=Audio\;%ID% "${fname}"`
-    `mkvextract tracks ${fname} ${VIDEO_TRACK}:${name}.h264 ${AUDIO_TRACK}:${name}.ac3 > /dev/null 2>&1`
+    VIDEO=`mediainfo --Inform=Video\;%ID% "${fname}"`
+    AUDIO=`mediainfo --Inform=Audio\;%ID% "${fname}"`
+    `mkvextract tracks ${fname} ${VIDEO}:${name}.h264 ${AUDIO}:${name}.ac3`
     `a52dec ${name}.ac3 -o wavdolby > ${name}.wav`
     `faac ${name}.wav -o ${name}.m4a`
     FPS=`mediainfo --Inform=Video\;%FrameRate% "${fname}"`
-    `MP4Box -add ${name}.m4a -add ${name}.h264 -fps $FPS ${name}.mp4 > /dev/null 2>&1`
+    `MP4Box -add ${name}.m4a -add ${name}.h264 -fps $FPS ${name}.mp4`
     `rm ${name}.m4a ${name}.ac3 ${name}.h264 ${name}.wav ${name}.mkv`
 fi
