99d6404edc5d3d8ba395bc26ac739fab83d69f74
Eric Himmelreich <eahimmel@gmail.com>
2010-12-11 23:48:51 -0500

    script now picks up any mkv files in the directory automatically

--- convert.sh
+++ convert.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
-if [ -f $1 ] ; then
-    filename=$(basename $1)
-    extension=${filename##*.}
-    name=${filename%.*}
-    video=`mediainfo --Inform=Video\;%ID% "${fname}"`
-    audio=`mediainfo --Inform=Audio\;%ID% "${fname}"`
-    fps=`mediainfo --Inform=Video\;%FrameRate% "${fname}"`
-    `mkvextract tracks ${fname} ${video}:${name}.h264 ${audio}:${name}.ac3`
-    `a52dec ${name}.ac3 -o wavdolby > ${name}.wav`
-    `faac ${name}.wav -o ${name}.m4a`
-    `MP4Box -add ${name}.m4a -add ${name}.h264 -fps $fps ${name}.mp4`
-    `rm ${name}.m4a ${name}.ac3 ${name}.h264 ${name}.wav ${name}.mkv`
-fi
+for f in *.mkv
+do
+    if [ -f $f ] ; then
+        filename=$(basename $f)
+        extension=${filename##*.}
+        name=${filename%.*}
+        video=`mediainfo --Inform=Video\;%ID% "${f}"`
+        audio=`mediainfo --Inform=Audio\;%ID% "${f}"`
+        fps=`mediainfo --Inform=Video\;%FrameRate% "${f}"`
+        `mkvextract tracks ${f} ${video}:${name}.h264 ${audio}:${name}.ac3`
+        `a52dec ${name}.ac3 -o wavdolby > ${name}.wav`
+        `faac ${name}.wav -o ${name}.m4a`
+        `MP4Box -add ${name}.m4a -add ${name}.h264 -fps $fps ${name}.mp4`
+        `rm ${name}.m4a ${name}.ac3 ${name}.h264 ${name}.wav`
+    fi
+done
+exit
